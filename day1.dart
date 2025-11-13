2ccaaa03cefc190b57c585dde98e7e25888d58b3
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-13 17:50:06 +0530

    first commit

---
+++ day1.dart
@@ -0,0 +1,42 @@
+void main() {
+  // print("Hello i am programmer dart language");
+  // int a = 8;
+  // int b = 5;
+  // String str1 = "Hello I am String";
+  // double d1 = 7.56;
+  // bool b1 = true;
+  // print(b1);
+  // print((d1));
+  // print(str1);
+  // print(a + b);
+  // int shivam45 = 78;
+  // int 45shivam = 78;
+  // print(shivam45);
+  // int adgdh
+  // int s1; // varible declaration
+  // s1 = 89; // initilization
+
+  // print(s1);
+  // s1 = 66; //update
+  // print(s1);
+  // print(s1);
+  // int name = 78; //
+  // name = 67;
+  // int Name = 56;
+  // print(name);
+  // int int1 = 67;
+  // int a = 78;
+  // int number1 = 89;
+  // print(number1);
+
+  //   var str1 = 67;
+  //   print(str1);
+  // dynamic str1 = "Shubham";
+  // str1 = 56;
+  // print((str1));
+  print("Hello i \nam good");
+  List<int> list56 = [4, 6, 7, 78];
+  print(list56[3]);
+  Map d1 = {"name": "Tayyab", "age": 34, "course": "B.Tech"};
+  print(d1);
+}
