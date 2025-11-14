1527e20d2f899782ac3a3dac82b4fdeef56ae071
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-14 16:23:08 +0530

    Operators

--- day1.dart
+++ day1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
   // print("Hello i am programmer dart language");
   // int a = 8;
@@ -34,9 +36,74 @@ void main() {
   // dynamic str1 = "Shubham";
   // str1 = 56;
   // print((str1));
-  print("Hello i \nam good");
-  List<int> list56 = [4, 6, 7, 78];
-  print(list56[3]);
-  Map d1 = {"name": "Tayyab", "age": 34, "course": "B.Tech"};
-  print(d1);
+
+  // print("Hello i \nam good");
+  // List<int> list56 = [4, 6, 7, 78];
+  // print(list56[3]);
+  // Map d1 = {"name": "Tayyab", "age": 34, "course": "B.Tech"};
+  // print(d1);
+
+  // double str1 = double.parse(stdin.readLineSync()!);
+  // print(str1);
+  // int a = 14;
+  // int b = 9;
+  // int c = a + b;
+  // print("The addition is: ${a + b}"); // 23
+  // print("The Subraction is: ${a - b}"); // 5
+  // print("The Multiply is: ${a * b}"); // 126
+  // print("The Divide is: ${a ~/ b}"); // 1
+  // print("The Modulo is: ${a % b}"); // 5
+
+  // print(a == b); // false
+  // print(a >= b); // true
+  // print(a < b); // false
+  // print(a <= b); // false
+  // print(a > b); //true
+  // print(a != b); // true
+
+  // int a = 14;
+  // int b = 9;
+  // print(a < b && a > 8);
+  // print(a > b || a > 8);
+  // print(!(a < b && a > 8));
+
+  // int a = 7;
+  // int b = 4;
+
+  // a = a + 7;
+  // a = a - 3;
+
+  // a += 7; //a=a+7
+  // print(a);
+  // a -= 5;
+  // print(a);
+  // a *= 5;
+  // print(a);
+  // a ~/= 45;
+  // print(a);
+
+  // int b = 8;
+  // print(++b);
+  // print(b);
+
+  // int a = 8;
+  // print(a); // 8  8
+  // print(--a); // 7  7
+  // print(a++); // 7  7
+  // print(++a); // 8  9
+  // print(a); //8  9
+  // print(--a); //7  8
+  // print(a--); // 7 8
+  // print(a); //6  //7
+  // print(++a); //6  8
+  // print(++a); //7  9
+  // print(a); //7  9
+
+  // int age = 67;
+  // String result = (age >= 18) ? 'Eligible' : 'Not Eligible';
+  // print(result);
+
+  // int num1 = 78;
+  // String s1 = "Shubham";
+  // print(num1 is! int);
 }
