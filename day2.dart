3dae490d58f5d3bfe0bfcc0a1fcc417d5c03285d
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-16 17:33:43 +0530

    strings nad list

---
+++ day2.dart
@@ -0,0 +1,48 @@
+void main() {
+  // int a;
+  // a = 9;
+  // a = 8;
+  // a = 67;
+  // int a = 90;
+
+  // String str1 = "Shivam";
+  // String str2 = 'Aman';
+  // str1 = "Rahul";
+  // print(str1);
+  // print(str2);
+  // int age = 60;
+  // print("I am neeraj my age is: ${age + 78}");
+  // String str1 = 'Neera\'j I eerajam going \\ to mumbai';
+  // print(str1);
+  // String str2 = """Neeraj
+  // I am going to mumbai
+  // hello i am good
+  // """;
+  // print(str2);
+  // String str1 = "Neeraj I am good good";
+  // print(str1.length);
+  // print(str1.toUpperCase());
+  // print(str1.toLowerCase());
+  // print(str1.contains("aj"));
+  // print(str1.startsWith("Nee"));
+  // print(str1.endsWith("aj"));
+  // print(str1.substring(1, 4));
+  // print(str1.substring(2, 5));
+  // String str2 = str1.replaceAll("good", "Shivam");
+  // print(str2);
+  // String str3 = "        Neeraj      ";
+  // print(str3.trim());
+  // String str4 = "Shivam is good";
+  // print(str4.split(" ").length);
+  List<int> list1 = [7, 4, 3, 7, 9];
+  // list1.add(67);
+  // list1.addAll([34, 78, 90]);
+  // list1.remove(34);
+  // list1.removeAt(2);
+  // print(list1.length);
+  // print(list1.contains(4));
+  // print(list1.indexOf(3));
+  // print(list1.reversed);
+  list1.clear();
+  print(list1);
+}
