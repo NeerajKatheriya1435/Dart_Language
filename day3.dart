c06fec28ecb18a3c3cd06fe3dc564170063341f6
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-18 09:26:07 +0530

    map and loops and condtionals

---
+++ day3.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // var str1 = "Shivam";
+  // str1 = 78;
+  // dynamic str1 = "Hello";
+  // str1 = 90;
+  // print(str1);
+  // {name:"Subhash",age:89}
+  // Map<String, dynamic> dict1 = {"Name": "Shivam", "Age": 45, "Weight": 45.78};
+  // print(dict1);
+  // print(dict1.keys);
+  // print(dict1.values);
+  // print(dict1.length);
+  // dict1.addAll({"Id": 101, "Roll Number": 19023456});
+  // print(dict1.remove("Roll Number"));
+  // print(dict1);
+  // print(dict1.containsKey("Id"));
+  // print(dict1.containsValue(101));
+  // dict1.clear();
+  // print(dict1);
+
+  // dict1.forEach(
+  //   (key, value) => {print("My key is: ${key} and value is: ${value}")},
+  // );
+  // print("Enter Your age:");
+  // int age = int.parse(stdin.readLineSync()!);
+  // if (age < 0 || age > 150) {
+  //   print("Invalid age");
+  // } else if (age > 0 && age < 18) {
+  //   print("You are Child You can't");
+  // } else if (age > 18 && age < 80) {
+  //   print("You can Drive the car");
+  // } else {
+  //   print("You are OverAged");
+  // }
+
+  // print("Enter Your age:");
+  // int age = int.parse(stdin.readLineSync()!);
+  // if (age > 34) {
+  //   print("Your value is greater then 34");
+  //   if (age == 56 || age == 78) {
+  //     print("You are Special");
+  //   }
+  // } else {
+  //   print("Your value is not greater then 34");
+  // }
+  // int day = 11;
+
+  // switch (day) {
+  //   case 11:
+  //     print("Sunday");
+  //   // break;
+  //   case 21:
+  //     print("Monday");
+  //   // break;
+  //   case 33:
+  //     print("Tuesday");
+  // break;
+  // default:
+  //   print("Enter Number Between 1 to 3");
+  // }
+}
