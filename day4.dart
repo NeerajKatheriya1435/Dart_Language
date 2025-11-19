e4bea094a4c07577af2df60a5f17e534eef2c9ba
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-19 16:43:29 +0530

    Loops

---
+++ day4.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+void main(List<String> args) {
+  // for loop
+
+  // int fact = 1;
+  // for (var i = 1; i <= 4; i++) {
+  //   fact *= i;
+  //   fact = fact* i;
+  // }
+  // print(fact);
+
+  // while loop
+
+  // int i = 1;
+  // while (i <= 11) {
+  //   if (i == 5) {
+  //     print(i);
+  //   }
+  //   i++;
+  // }
+  // int j = 111;
+  // do {
+  //   print(j);
+  //   j++;
+  // } while (j < 11);
+
+  // var list = [4, 5, 6, 8, 3];
+  // print(list);
+  // print(list[3]);
+  // for (var i = 0; i < list.length; i++) {
+  //   if (i == 3) {
+  //     break;
+  //   }
+  //   print(list[i]);
+  // }
+
+  // for (var element in list) {
+  //   print(element);
+  // }
+
+  // list.forEach((element) => {print(element * element)});
+}
